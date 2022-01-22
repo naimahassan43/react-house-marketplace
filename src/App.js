22591189e81f2448a0b44bd868a9dbf10b61445a
Naima Hassan <naima.hassan434@gmail.com>
2022-01-22 00:40:10 +0000

    Navbar component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Profiles from './pages/Profiles'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -22,8 +23,9 @@ function App() {
           <Route path="/sign-up" element={<SignUp />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
         </Routes>
+         <Navbar/>
       </Router>
-      {/* Navbar */}
+      
     </>
   );
 }
