9e3cb0b5dfadcdf2989e1c8248e65d9ed2795940
Naima Hassan <naima.hassan434@gmail.com>
2022-02-12 21:33:12 +0000

    Display & Update User Details

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
-import Profiles from './pages/Profiles'
+import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
@@ -20,7 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />}/>
           <Route path="/offers" element={<Offers />}/>
-          <Route path="/profile" element={<Profiles />}/>
+          <Route path="/profile" element={<Profile />}/>
           <Route path="/sign-in" element={<SignIn />}/>
           <Route path="/sign-up" element={<SignUp />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
