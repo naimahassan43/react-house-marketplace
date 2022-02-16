4024155ea248adc296abbb8aa8c6c645360b7194
Naima Hassan <naima.hassan434@gmail.com>
2022-02-16 00:44:27 +0000

    Get Coords With Geocoding API

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,6 +3,9 @@ import React,{ useState, useEffect, useRef} from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import { useNavigate } from 'react-router-dom'
 import Spinner from '../components/Spinner'
+import { toast } from 'react-toastify'
+
+//Api key = AIzaSyC0D1PR67vFG8V50TF9p_40mpINGFIBHQY
 
 function CreateListing() {
    const [geolocationEnabled, setGeolocationEnabled] = useState(true)
@@ -59,8 +62,50 @@ function CreateListing() {
       }
    }, [isMounted])
 
-   const onSubmit = e => {
+   const onSubmit = async(e) => {
       e.preventDefault()
+
+      setLoading(true)
+
+      if (discountedPrice >= regularPrice) {
+         setLoading(false)
+         toast.error('Discounted price needs to be less than regular price')
+         return 
+      }
+
+      if (images.length > 6) {
+         setLoading(false)
+         toast.error('Max 6 images')
+         return
+      }
+
+      let geolocation = {}
+      let location
+
+      if (geolocationEnabled) {
+         const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`)
+         
+         const data = await response.json()
+
+         geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+         geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+         location = data.status === 'ZERO_RESULTS' ? undefined : data.results[0]?.formatted_address
+
+         if (location === undefined || location.includes('undefined')) {
+            setLoading(false)
+            toast.error('Please enter a correct address')
+            return
+         }
+      
+      } else {
+         geolocation.lat = latitude
+         geolocation.lng = longitude
+         location = address
+         
+      }
+
+      setLoading(false)
    }
 
    const onMutate = e => {
@@ -187,6 +232,34 @@ function CreateListing() {
                   No
                   </button>
                </div>
+
+               <label className='formLabel'>Furnished</label>
+               <div className='formButtons'>
+                  <button
+                     className={furnished ? 'formButtonActive' : 'formButton'}
+                     type='button'
+                     id='furnished'
+                     value={true}
+                     onClick={onMutate}
+                     >
+                     Yes
+                  </button>
+                  <button
+                     className={
+                        !furnished && furnished !== null
+                           ? 'formButtonActive'
+                           : 'formButton'
+                     }
+                     type='button'
+                     id='furnished'
+                     value={false}
+                     onClick={onMutate}
+                     >
+                     No
+                  </button>
+               </div>
+
+
                      
                <label className='formLabel'>Address</label>
                <textarea
