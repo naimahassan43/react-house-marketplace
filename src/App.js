d7870472adfeda6a2122e30cf6db0745367dffcf
Naima Hassan <naima.hassan434@gmail.com>
2022-02-16 23:54:07 +0000

    Fetch Single Listing

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Navbar from "./components/Navbar";
 import PrivateRoute from "./components/PrivateRoute";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
+import Listing from "./pages/Listing";
 
 function App() {
   return (
@@ -30,7 +31,8 @@ function App() {
           <Route path="/sign-in" element={<SignIn />}/>
           <Route path="/sign-up" element={<SignUp />}/>
           <Route path="/forgot-password" element={<ForgotPassword />}/>
-          <Route path="/create-listing" element={<CreateListing/>}/>
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing/>}/>
         </Routes>
         <Navbar />
         
