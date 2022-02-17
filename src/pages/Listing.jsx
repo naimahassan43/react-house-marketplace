c375c4aed103b35d74943e59122242d425291316
Naima Hassan <naima.hassan434@gmail.com>
2022-02-17 19:59:55 +0000

    Leaflet Map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect} from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { getDoc, doc } from 'firebase/firestore'
 import { getAuth} from 'firebase/auth'
-import {db} from "../firebase.config"
+import { db } from "../firebase.config"
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import Spinner from '../components/Spinner'
 import shareIcon from '../assets/svg/shareIcon.svg'
 
@@ -75,6 +76,18 @@ function Listing() {
 
             <p className="listingLocationTitle">Location</p>
             {/* Map */}
+            <div className="leafletContainer">
+               <MapContainer style={{height: '100%', width: '100%'}} center = {[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}>
+                  
+                  <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+                  />
+                  <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+                     <Popup>{listing.location }</Popup>
+                  </Marker>
+               </MapContainer>
+            </div>
 
             {auth.currentUser?.uid !== listing.userRef && (
                <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className="primaryButton">Contact Landlord</Link>  
