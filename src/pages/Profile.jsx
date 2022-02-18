fea75034bbf3c512c6155d77c9f00457630cea43
Naima Hassan <naima.hassan434@gmail.com>
2022-02-18 01:44:56 +0000

    Edit Listing Icon

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -99,6 +99,9 @@ function Profile() {
       toast.success("Successfully deleted listing");
     }
   };
+
+  const onEdit = (listingId) => navigate(`/edit-listing/${listingId}`);
+
   return (
     <div className="profile">
       <header className="profileHeader">
@@ -156,6 +159,7 @@ function Profile() {
                   listing={listing.data}
                   id={listing.id}
                   onDelete={() => onDelete(listing.id)}
+                  onEdit={() => onEdit(listing.id)}
                 />
               ))}
             </ul>
